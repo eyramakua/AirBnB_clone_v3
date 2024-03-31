5824b88a5187e4d1270a8c19512f8b431b575b7d
eyramakua <eyramagginie@gmail.com>
2024-03-31 14:52:13 -0700

    task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,28 +3,17 @@
 module to start using api
 """
 
-from flask import Flask, jsonify, make_response
-from flask_cors import CORS
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 app = Flask(__name__)
-CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
-app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
 def exit(exception):
     '''exit api in case of unexpected error'''
     storage.close()
-
-
-@app.errorhandler(404)
-def error404(e):
-    '''instance to handle error 404'''
-    response = {"error": "Not found"}
-    return make_response(jsonify(response), 404)
 
 
 if __name__ == '__main__':
