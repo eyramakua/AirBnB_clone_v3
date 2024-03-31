cb893dea6dd872b5be218db87cfbad9abbf440ff
eyramakua <eyramagginie@gmail.com>
2024-03-31 14:24:09 -0700

    task 3

---
+++ api/v1/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+module to start using api
+"""
+
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+app = Flask(__name__)
+CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
+@app.teardown_appcontext
+def exit(exception):
+    '''exit api in case of unexpected error'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    host = getenv("HBNB_API_HOST")
+    port = getenv("HBNB_API_PORT")
+    if host is None:
+        host = '0.0.0.0'
+    if port is None:
+        port = 5000
+    app.run(host=host, port=port)
