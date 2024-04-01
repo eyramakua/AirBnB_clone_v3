3c99b85f2b3d68abf75a44731f4fe20d4ed209b8
eyramakua <eyramagginie@gmail.com>
2024-04-01 00:23:08 -0700

    corrections of task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,8 +7,11 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+
+
 app = Flask(__name__)
 
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def exit(exception):
